6a901176fe1975d5b27cecc02a548a40a9d973dd
ruviRedner <r.y.r7966@gmail.com>
2024-11-14 13:57:06 +0200

    creating slice action and func for fetch action

---
+++ src/redux/slices/actionSlice.ts
@@ -0,0 +1,61 @@
+import {
+    ActionReducerMapBuilder,
+    createAsyncThunk,
+    createSlice
+  } from '@reduxjs/toolkit';
+  import { actionState, DataStatus } from '../types/redux';
+  
+  import { Iaction } from '../types/Iaction';
+  
+  const initialState: actionState = {
+    error: null,
+    status: DataStatus.IDEL,
+    data: []
+  };
+  
+  export const fatchAction = createAsyncThunk(
+    '/action/getAction',
+    async (_, thunkApi) => {
+    //   const token = localStorage.getItem('token');
+      try {
+        const res = await fetch('http://localhost:7966/action/getAction', 
+        //   headers: {
+        //     Authorization: token!
+        //   }
+        );
+        if (res.status !== 200) thunkApi.rejectWithValue("Can't get list");
+        const data = await res.json();
+        //  return thunkApi.fulfillWithValue(data)
+        return data;
+      } catch (error) {
+        thunkApi.rejectWithValue('you are switte');
+      }
+    }
+  );
+  
+  export const actionSlice = createSlice({
+    name: 'action',
+    initialState,
+    reducers: {},
+    extraReducers: (builder: ActionReducerMapBuilder<actionState>) => {
+      builder
+        .addCase(fatchAction.pending, (state) => {
+          state.status = DataStatus.LOADING;
+          state.error = null;
+          // state.data = [];
+        })
+        .addCase(fatchAction.fulfilled, (state, action) => {
+          state.status = DataStatus.SUCCESS;
+          state.error = null;
+          state.data = action.payload as unknown as Iaction[];
+        })
+        .addCase(fatchAction.rejected, (state, action) => {
+          state.status = DataStatus.FAILED;
+          state.error = action.error as string;
+          state.data = [];
+        });
+    }
+  });
+  
+  export default actionSlice;
+  
