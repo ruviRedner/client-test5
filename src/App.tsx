de1751ce4e98092d521d0b9076c812b74144d47d
ruviRedner <r.y.r7966@gmail.com>
2024-11-14 13:53:18 +0200

    basic app done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./components/auth/Login";
+
+import Attack from "./components/pages/Attack";
+import Deffence from "./components/pages/Deffence";
+import EnemyStore from "./components/pages/EnemyStore";
+import IdfStore from "./components/pages/IdfStore";
+import Register from "./components/auth/Register";
+import { useAppDispatch } from "./redux/store";
+import userSlice from "./redux/slices/userSlice";
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  useEffect(()=>{
+    const user = localStorage.getItem("user")
+    if(user){
+      dispatch(userSlice.actions.setUser(JSON.parse(user)))
+    }
+  },[dispatch])
   return (
     <div>
-      
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="attack" element={<Attack />} />
+        <Route path="deffence" element={<Deffence />} />
+        <Route path="enemy-store" element={<EnemyStore />} />
+        <Route path="idf-store" element={<IdfStore />} />
+        <Route path="/" element={<Navigate to={"/login"} />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
